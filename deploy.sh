9d9cce63749ddcdbda25aeb8d510796243f6788a
daniel.mueller <daniel.mueller@insitu.de>
2022-10-28 12:28:50 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit
+git pull
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+MIX_ENV=prod mix assets.deploy
+MIX_ENV=prod mix release --overwrite
+MIX_ENV=prod mix ecto.migrate
